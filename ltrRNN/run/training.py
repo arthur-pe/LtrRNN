e0abde443f1f88beb20f7d532a1cc7c1dde7e5d3
arthur <pellegrino.arthur@gmail.com>
2024-02-20 20:42:13 +0100

    Live figure Colab

--- ltrRNN/run/training.py
+++ ltrRNN/run/training.py
@@ -172,11 +172,11 @@ def training_loop(sde_preparatory, sde_execution, net, condition_wise_map, rnn_t
                 plt.savefig(directory + '/' + directory.split('/')[-1] + '.pdf')
                 if training_iteration == 0: plt.savefig(directory + '/' + directory.split('/')[-1] + '-pre-training.pdf')
 
-                plt.pause(5) # Increase for some CPU configs
-
-                '''if 'google.colab' in sys.modules:
+                if 'google.colab' in sys.modules:
                     clear_output()
-                    display(fig)'''
+                    display(fig)
+                else:
+                    plt.pause(5)  # Increase for some CPU configs
 
                 torch.save(sde_preparatory.state_dict(), directory + '/model.pt')
                 torch.save(rnn_to_data.state_dict(), directory + '/map.pt')
